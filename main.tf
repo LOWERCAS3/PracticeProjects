52e2de5815f345df5b5bb5c1291f2842d904f188
Sanjeet Shekhar <43709835+LOWERCAS3@users.noreply.github.com>
2022-05-24 17:25:25 +0530

    Update main.tf
    Testing main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
-
 terraform {
-  # Require Terraform version 0.13.x (recommended)
-  required_version = "~> 0.13.0"
-
-  # Require the latest 2.x version of the New Relic provider
   required_providers {
     newrelic = {
-      source  = "newrelic/newrelic"
-      version = "~> 2.21"
+      source = "newrelic/newrelic"
+      version = "2.46.0"
     }
   }
+}
+
+provider "newrelic" {
+  # Configuration options
 }
